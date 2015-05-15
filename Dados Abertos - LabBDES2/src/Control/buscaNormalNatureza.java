4654ab2f24852fb4a31590189e8fa1f06ef8674d
Rick <japa.eihara@gmail.com>
2015-05-15 16:53:18 -0300

    Controle parcialmente implementado...

---
+++ Dados Abertos - LabBDES2/src/Control/buscaNormalNatureza.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+import Model.buscaNormal;
+import java.sql.*;
+import java.util.Vector;
+
+/**
+ *
+ * @author floss
+ */
+public class buscaNormalNatureza {
+    private Vector<buscaNormal> resultados;
+    private ConexaoBD con;
+    
+    buscaNormalNatureza(ConexaoBD con){
+        
+        
+    }
+    
+    
+}
