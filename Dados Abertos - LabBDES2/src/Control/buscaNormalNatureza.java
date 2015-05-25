990e4b8fbd1f961a952b1d3106bfd84013507737
Rick <japa.eihara@gmail.com>
2015-05-25 12:44:43 -0300

    Implementado as novas consultas básicas Feito também a consulta de municípios

--- Dados Abertos - LabBDES2/src/Control/buscaNormalNatureza.java
+++ Dados Abertos - LabBDES2/src/Control/buscaNormalNatureza.java
@@ -11,18 +11,17 @@ public class buscaNormalNatureza {
         conexao = con;
     }
 
-    public ArrayList<buscaNormal> getBuscaNormalPrograma(String descricao){
+    public ArrayList<buscaNormal> getBuscaNormalPrograma(String equipamento, String municipio){
 	    ArrayList<buscaNormal> resultado = new ArrayList<buscaNormal>();
 	    ResultSet rs = null;
 	    buscaNormal aux = null;
 	    String texto_consulta =
-		"SELECT P.codigo, P.descricaointernamunicipio, SUM(Desp.valor) AS gasto "
-		+ "FROM despesa Desp, programa P, ("
-		    	+ "SELECT M.codigo  FROM municipio M WHERE M.descricao = '" +descricao+ "'"
-		+ ") Mun WHERE Mun.codigo = Desp.codigomunicipio "
-		+ "AND P.codigo = Desp.codigoprograma "
-		+ "GROUP BY P.codigo, P.descricaointernamunicipio "
-		+ "ORDER BY gasto DESC;";
+		"SELECT  N.codigo, N.descricao, SUM(Desp.valor) AS gasto FROM despesa Desp, ("
+		    + "SELECT codigo, descricao FROM natureza WHERE descricao ILIKE '%"+equipamento+"%'"
+		+ ")N, ("
+		    + "SELECT M.codigo FROM municipio M WHERE M.descricao = '"+municipio+"'"
+		+ ")Mun WHERE Mun.codigo = Desp.codigomunicipio AND N.codigo = Desp.codigonatureza "
+		+ "GROUP BY N.codigo, N.descricao ORDER BY gasto DESC;";
 
 	    System.out.println(texto_consulta);
 
@@ -45,20 +44,17 @@ public class buscaNormalNatureza {
 	    return resultado;
     }
   
-    public ArrayList<buscaNormal> getBuscaNormalNatureza(String descricao){
+    public ArrayList<buscaNormal> getBuscaNormalNatureza(String fomento, String municipio){
 	    ArrayList<buscaNormal> resultado = new ArrayList<buscaNormal>();
 	    ResultSet rs = null;
 	    buscaNormal aux = null;
 	    String texto_consulta =
-		"SELECT N.codigo, N.descricao, SUM(Desp.valor) AS gasto "
-		+ "FROM despesa Desp, natureza N, ("
-		    + "SELECT M.codigo "
-		    + "FROM municipio M "
-		    + "WHERE M.descricao = '"+ descricao +"') Mun "
-		+ "WHERE Mun.codigo = Desp.codigomunicipio AND "
-		+ "N.codigo = Desp.codigonatureza "
-		+ "GROUP BY N.codigo, N.descricao "
-		+ "ORDER BY gasto DESC";
+		"SELECT  P.codigo, P.descricaointernamunicipio, SUM(Desp.valor) AS gasto FROM despesa Desp, ("
+		    + "SELECT codigo, descricaointernamunicipio FROM programa WHERE descricaointernamunicipio ILIKE '%"+fomento+"%'"
+		+ ")P, ("
+		    + "SELECT M.codigo FROM municipio M WHERE M.descricao = '"+municipio+"'"
+		+ ")Mun WHERE Mun.codigo = Desp.codigomunicipio AND P.codigo = Desp.codigoprograma "
+		    + "GROUP BY P.codigo, P.descricaointernamunicipio ORDER BY gasto DESC;";
 
 	    System.out.println(texto_consulta);
 
