adb83914c8e6a05312d140addd9b68aa069a716b
rick <hue@hue.com.br>
2015-06-10 17:23:10 -0300

    Feito alterações no programa para funcionar tabela temporária

--- Dados Abertos - LabBDES2/src/Control/ConexaoBD.java
+++ Dados Abertos - LabBDES2/src/Control/ConexaoBD.java
@@ -1,5 +1,7 @@
 package Control;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConexaoBD {
     //Objeto que guarda informacoes da conexao com o SGBD.
@@ -24,13 +26,21 @@ public class ConexaoBD {
             Class.forName("org.postgresql.Driver").newInstance();
             
             myConnection = DriverManager.getConnection("jdbc:postgresql:" +
-                    "//localhost:5432/trabalho?user=postgres&password=postgres");
+                    "//localhost:5432/projlabbd?user=postgres&password=1234");
             st = myConnection.createStatement();
             setConectado(true);
         }
         catch (Exception e){
             setConectado(false);
             e.printStackTrace();
+        }
+        
+        String texto_consulta = 
+                "CREATE TEMPORARY TABLE TabConsultaAvancada(codMunicipio INTEGER,codPrograma INTEGER,codNatureza INTEGER,anoInic INTEGER,gasto NUMERIC);INSERT INTO TabConsultaAvancada SELECT d.codigomunicipio, d.codigoprograma, d.codigonatureza, d.dataano, SUM(d.valor) FROM despesa d, programa p, natureza n, municipio m WHERE m.codigo = d.codigomunicipio AND d.codigoprograma = p.codigo AND d.codigonatureza = n.codigo GROUP BY d.codigomunicipio, d.codigoprograma, d.codigonatureza, d.dataano;CREATE OR REPLACE VIEW ConsultaAvancada(munic, progr, natu, data, gasto) AS SELECT * FROM TabConsultaAvancada;";
+        try {
+            st.execute(texto_consulta);
+        } catch (SQLException e) {
+            System.out.println(e);
         }
     }
 }
